6703ccff9aa357c6bdd4eef786815c4302a1a35e
中村修太 <nakamurashuuta@nakamurashuutanoMacBook-Air.local>
2021-03-30 15:27:07 +0900

    fix log

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  console.log(Deno.env.get("DENO_DEPLOYMENT_ID"));
+  console.log("DENO_DEPLOYMENT_ID:" + Deno.env.get("DENO_DEPLOYMENT_ID"));
 
   const response = new Response("Hello Deno Deploy from main.js!", {
     headers: { "content-type": "text/plain" },
