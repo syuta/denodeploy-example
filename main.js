af59f2d7522ba29ad647088b4c99bd6718d9ecca
中村修太 <nakamurashuuta@nakamurashuutanoMacBook-Air.local>
2021-03-30 15:23:04 +0900

    add log

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 addEventListener("fetch", (event) => {
+  console.log(Deno.env.get("DENO_DEPLOYMENT_ID"));
+
   const response = new Response("Hello Deno Deploy from main.js!", {
     headers: { "content-type": "text/plain" },
   });
