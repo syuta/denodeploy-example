bb969e469d9afe7fce1ce96c738d5b9df6d32071
中村修太 <nakamurashuuta@nakamurashuutanoMacBook-Air.local>
2021-04-01 11:18:25 +0900

    add env

--- tsmain.ts
+++ tsmain.ts
@@ -1,5 +1,12 @@
+class Env {
+    getVar(key:string):any {
+        return Deno.env.get(key);
+    }
+}
+
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello Deno Deploy from tsmain.ts!", {
+  const msg = new Env().getVar("FOO");
+  const response = new Response("FOO : " + msg, {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
