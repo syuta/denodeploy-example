7fa3320b72c5cbcb0e96d0d93f0337a4ad945142
中村修太 <nakamurashuuta@nakamurashuutanoMacBook-Air.local>
2021-04-02 11:25:20 +0900

    fix

--- tsmain.ts
+++ tsmain.ts
@@ -1,15 +1,7 @@
 
-class Env {
-    getVal(key:string):any {
-        return Deno.env.get(key);
-    }
-}
-
-const env = new Env();
-
 function handleRequest(request:Request) {
   const pathname:URL = new URL(request.url);
-  const value:string = env.getVal("FOO");
+  const value:string | undefined = Deno.env.get("FOO");
   const txt = ` Hello DenoDeploy!\n path : ${pathname}\n value:${value}\n`;
   return new Response(txt, {
     headers: { "content-type": "text/plain" },
